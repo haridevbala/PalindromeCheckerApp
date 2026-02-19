f78932689a7df6052b210359665db3915baa200c
C BHAVYA SREE SAI <>
2026-02-19 10:01:31 +0530

    Base application setup

---
+++ src/Palindromechecker.java
@@ -0,0 +1,7 @@
+public class Palindromechecker {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Palindrome Checker Management system");
+        System.out.println("Version : 1.0");
+        System.out.println("System Initialized Successfully");
+    }
+}
