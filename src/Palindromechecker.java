f3156027c8ce47a11ace553544bb160aecfb4cd2
C BHAVYA SREE SAI <>
2026-02-19 10:22:46 +0530

    UC2:Hardcoded palindrome

--- src/Palindromechecker.java
+++ src/Palindromechecker.java
@@ -3,5 +3,18 @@ public class Palindromechecker {
         System.out.println("Welcome to the Palindrome Checker Management system");
         System.out.println("Version : 1.0");
         System.out.println("System Initialized Successfully");
+        System.out.println("________________________________");
+        String input = "madam";
+        boolean isPalindrome = true;
+        for (int i = 0;i < input.length()/2;i++) {
+            if(input.charAt(i) != input.charAt(input.length() - 1 - i)){
+                isPalindrome = false;
+                break;
+
+            }
+        }
+        System.out.println("Input text:" + input);
+        System.out.println("Is it a Palindrome?:" + isPalindrome);
+
     }
 }
